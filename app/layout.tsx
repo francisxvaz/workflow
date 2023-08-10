f996c21bff90d0249f1646a279cde027fb6d13c6
Francis Vaz <francisxvaz@gmail.com>
2023-08-10 20:22:04 +1000

    barebone

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+       <body>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Header/>
+            {children}
+          </ThemeProvider>
+        </body>
     </html>
   )
 }
